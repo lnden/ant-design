3d335cf6e57bc8d46463a72c804fa2fa2dd8330e
lnden <258231674@qq.com>
2019-07-18 13:52:10 +0800

    new add page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,11 +1,18 @@
 import { Layout, Menu } from 'antd';
+import Link from 'umi/link';
 import styles from './index.css';
 
 const { Header, Content, Footer } = Layout;
 const { Item } = Menu;
 
+const menuData = [
+    { route: 'hero', name: '英雄' },
+    { route: 'item', name: '局内道具' },
+    { route: 'summoner', name: '召唤师技能' },
+];
 
 function BasicLayout(props) {
+    const { location: { pathname }, children } = props;
     return (
         <Layout>
             <Header>
@@ -13,16 +20,20 @@ function BasicLayout(props) {
                 <Menu
                     theme="dark"
                     mode="horizontal"
-                    defaultSelectedKeys={['1']}
+                    defaultSelectedKeys={[pathname]}
                     style={{ lineHeight: '64px' }}
                 >
-                    <Item key="1">英雄</Item>
-                    <Item key="2">局内道具</Item>
-                    <Item key="3">召唤技能</Item>
+                    {
+                        menuData.map(menu => (
+                            <Item key={`/${menu.route}`}><Link to={menu.route}>{menu.name}</Link></Item>
+                        ))
+                    }
                 </Menu>
             </Header>
             <Content style={{ padding: '0 50px' }}>
-                {props.children}
+                <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+                    {children}
+                </div>
             </Content>
             <Footer style={{ textAlign: 'center' }}>
                 Umi 入门教程 Created by 2018/07/18
