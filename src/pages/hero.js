5a16ce7210bc7350b7dc54460904a00b0a69b8e5
lnden <258231674@qq.com>
2019-07-18 15:03:48 +0800

    add request

--- src/pages/hero.js
+++ src/pages/hero.js
@@ -2,11 +2,11 @@ import { connect } from 'dva'
 import styles from './hero.css';
 
 function Hero(props) {
-    console.log(props.hero)
+    // console.log(props.hero.heros)
     return (
         <div className={styles.normal}>
             <h1>Page hero</h1>
-            <h2>This is {props.hero}</h2>
+            <h2>This is {JSON.stringify(props.hero)}</h2>
         </div>
     );
 }
