e4195adbaba9ac9f4399d2c364a5adad87e38d58
lnden <258231674@qq.com>
2019-07-18 17:47:23 +0800

    code recons

--- src/models/item.js
+++ src/models/item.js
@@ -1,13 +1,33 @@
+import { queryItem } from '../services/api';
+
 export default {
     name: 'item',
-    state: 'item',
+    state: {
+        items: [],
+    },
+    subscriptions: {
+        setup({ dispatch, history }) {
+            return history.listen(({ pathname, query }) => {
+                if (pathname === '/item') {
+                    dispatch({
+                        type: 'fetch',
+                    });
+                }
+            });
+        },
+    },
     reducers: {
-
+        save(state, { payload }) {
+            return { ...state, ...payload };
+        },
     },
     effects: {
-
+        * fetch({ type, payload }, { put, call, select }) {
+            const item = yield call(queryItem);
+            yield put({
+                type: 'save',
+                payload: item,
+            });
+        },
     },
-    subscriptions:{
-
-    }
-}
+};
--- src/models/summoner.js
+++ src/models/summoner.js
@@ -1,7 +1,33 @@
+import { querySummoner } from '../services/api';
+
 export default {
     name: 'summoner',
-    state: 'summoner',
-    reducers: {},
-    effects: {},
-    subscriptions: {}
-}
+    state: {
+        summoners: [],
+    },
+    subscriptions: {
+        setup({ dispatch, history }) {
+            return history.listen(({ pathname, query }) => {
+                if (pathname === '/summoner') {
+                    dispatch({
+                        type: 'fetch',
+                    });
+                }
+            });
+        },
+    },
+    reducers: {
+        save(state,{payload}){
+            return {...state,...payload}
+        }
+    },
+    effects: {
+        * fetch({ type, payload }, { call, put, select }) {
+            const summoner = yield call(querySummoner);
+            yield put({
+                type: 'save',
+                payload:summoner
+            })
+        },
+    },
+};
