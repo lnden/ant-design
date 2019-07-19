9bf5573fe2a7e71f1928a140c2dfbac021de5645
lnden <258231674@qq.com>
2019-07-19 11:22:22 +0800

    change herodetail

--- src/pages/herodetails/$ename.js
+++ src/pages/herodetails/$ename.js
@@ -1,16 +1,16 @@
-import styles from './index.css';
-import Link from 'umi/link'
-import { Button } from 'antd'
+import Link from 'umi/link';
+import { Button } from 'antd';
+import styles from './index.less';
 
 export default function({ match }) {
-    const { params:{ename}} = match
+    const { params: { ename } } = match;
     return (
-        <main>
-            <Button type="primary"><Link to="/hero">返回上一页面</Link></Button>
+        <section>
+            <div className={styles.img}
+                 style={{ backgroundImage: `url("http://game.gtimg.cn/images/yxzj/img201606/skin/hero-info/${ename}/${ename}-bigskin-1.jpg")` }} alt={{ename}}></div>
             <div className={styles.normal}>
-                <h1>Page index <span style={{color:'#f00'}}>{ename}</span></h1>
+                <Button type="primary"><Link to="/hero">返回上一页面</Link></Button>
             </div>
-        </main>
-
+        </section>
     );
 }
